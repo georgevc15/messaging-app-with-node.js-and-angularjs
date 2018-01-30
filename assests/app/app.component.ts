7964d24fb37b284f5f203fe82e06f7ff97fe9073
George Codat Tudoran <office@solcreation.ro>
2018-01-30 19:59:49 +0200

    adding a development workflow script

--- assests/app/app.component.ts
+++ assests/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
-	selector: 'my-app',
-	templateUrl: './app.component.html',
-	styleUrls: ['app.componet.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.componet.css']
 })
 
 export class AppComponent {
