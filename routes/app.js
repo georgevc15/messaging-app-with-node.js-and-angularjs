119e3c3c2490c754e22a24999bfee1d110c4bf17
George Codat Tudoran <office@solcreation.ro>
2018-02-11 13:19:31 +0200

    Storing data into database

--- routes/app.js
+++ routes/app.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/message/:msg', function(req, res, next) {
-  res.render('node', {message: req.params.msg});
+*/
+router.get('/', function(req, res, next) {
+  res.render('node');
 });
 
-router.post('/message', function(req, res, next) {
-		var message = req.body.message;
-		res.redirect('/message/' + message);
+router.post('/', function(req, res, next) {
+		var email = req.body.email;
+		var user = new User({
+			firstName: 'George',
+			lastName: 'Tudoran',
+			password: 'secret',
+			email: email
+		});
+
+		user.save();
+
+		res.redirect('/');
 });
 
 module.exports = router;
