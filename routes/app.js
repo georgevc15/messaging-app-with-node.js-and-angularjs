c53eac7af11a3c3c275736ec1541a880c2a31e14
George Codat Tudoran <office@solcreation.ro>
2018-02-13 22:52:12 +0200

    Fetching data from db

--- routes/app.js
+++ routes/app.js
@@ -8,7 +8,12 @@ router.get('/', function(req, res, next) {
 });
 */
 router.get('/', function(req, res, next) {
-  res.render('node');
+  User.findOne({}, function (err, doc) {
+  	if(err) {
+  		return res.send('Error');
+  	}
+  	res.render('node', {email:doc.email});
+  });
 });
 
 router.post('/', function(req, res, next) {
