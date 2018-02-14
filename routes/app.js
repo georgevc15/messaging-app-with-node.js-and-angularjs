62672266c506e694d337e507c7c386d3ec517add
George Codat Tudoran <office@solcreation.ro>
2018-02-14 22:01:49 +0200

    rendering angular app with nodejs/express

--- routes/app.js
+++ routes/app.js
@@ -1,33 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
-/* GET home page.
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-*/
-router.get('/', function(req, res, next) {
-  User.findOne({}, function (err, doc) {
-  	if(err) {
-  		return res.send('Error');
-  	}
-  	res.render('node', {email:doc.email});
-  });
-});
-
-router.post('/', function(req, res, next) {
-		var email = req.body.email;
-		var user = new User({
-			firstName: 'George',
-			lastName: 'Tudoran',
-			password: 'secret',
-			email: email
-		});
-
-		user.save();
-
-		res.redirect('/');
+  	res.render('index');
 });
 
 module.exports = router;
