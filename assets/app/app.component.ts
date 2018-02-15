250fd6205385ee32b7fad2290b92a82c407ef996
George Codat Tudoran <office@solcreation.ro>
2018-02-15 23:22:40 +0200

    starting building templates and styles

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+	message =  {
+		content: 'A test meesage',
+		author: 'Max'
+	};
 }
