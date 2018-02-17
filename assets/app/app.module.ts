e8d741ee7f830e940e73d46b7b9b1b9f71530b7f
George Codat Tudoran <office@solcreation.ro>
2018-02-17 21:04:27 +0200

    improving folder structure

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import { MessageComponent } from "./messages/message.component";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+    AppComponent,
+    MessageComponent
+    ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
 })
