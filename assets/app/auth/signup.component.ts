ee71b227559d4636294eee224abc27c2c539414d
George Codat Tudoran <office@solcreation.ro>
2018-03-13 21:37:32 +0200

    Adding child routes

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
 	selector: 'app-signup',
-	templateUrl: './signnup.component.html'
+	templateUrl: './signup.component.html'
 })
 
 export class SignupComponent {
