f6bdf2a988cdbaba0d230b7d5033f78baf56b824
George Codat Tudoran <office@solcreation.ro>
2018-03-15 22:54:49 +0200

    Adding signup form

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 
 import { AppComponent } from "./app.component";
@@ -30,7 +30,7 @@ import { routing } from "./app.routing";
     SignupComponent,
     SigninComponent
     ],
-    imports: [BrowserModule, FormsModule, routing],
+    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
     bootstrap: [AppComponent]
 })
 
