1ff7f566107a6559cfe64e0d2c59db8961bafa7d
George Codat Tudoran <office@solcreation.ro>
2018-05-08 18:43:07 +0300

    Sign in back end

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
-var User = require('../model/user');
+var User = require('../models/user');
 
 router.post('/', function(req, res, next) {
   	var user = new User({
@@ -23,6 +24,34 @@ router.post('/', function(req, res, next) {
          	obj: result
          });
   	});
+});
+
+router.post('/signin', function(req, res, next) {
+	User.findOne({email: req.body.email}, function(err, user) {
+		  if(err) {
+            return res.status(401).json({
+            title: 'An error occured',
+            error: err
+          });
+         }
+         if(!user) {
+         	return res.status(401).json({
+         		title: 'Login failed',
+         		error: {message: 'Invalid login credentials'}
+         	});
+         }
+         if(!bycrypt.compareSync(req.body.password, user.password)) {
+         	 return res.status(401).json({
+         		title: 'Login failed',
+         		error: {message: 'Invalid login credentials'}
+         	});
+          }
+          var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+		  res.status(200).json({
+		  	message: 'Successfully logged in',
+		  	userId: user._id	
+		  }); 	
+	});
 });
 
 module.exports = router;
